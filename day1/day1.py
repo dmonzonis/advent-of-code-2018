75c6aca49bdb27345fb6a78576636ed8ada0707e
monzo94 <dmonzonis@protonmail.com>
2019-01-20 11:25:03 +0100

    Add day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -5,11 +5,26 @@ def apply_changes(change_list):
     return result
 
 
+def find_first_duplicate(change_list):
+    seen = {0}
+    current = 0
+    while True:
+        for c in change_list:
+            current += c
+            if current in seen:
+                return current
+            seen.add(current)
+
+
 def main():
     with open("input") as f:
         change_list = [int(c) for c in f.readlines()]
 
+    # Part 1
     print(apply_changes(change_list))
+
+    # Part 2
+    print(find_first_duplicate(change_list))
 
 
 if __name__ == "__main__":
