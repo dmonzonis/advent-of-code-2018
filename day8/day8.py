7dc46b0113309dfad55fdc1e0f3536b93095bca8
monzo94 <dmonzonis@protonmail.com>
2019-02-02 17:06:31 +0100

    Add day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,27 +1,26 @@
 class Tree:
     def __init__(self):
-        self._node = 0
+        self._max_node = 0
         self._tree = {}
 
     def build_tree(self, data):
-        self._parse_node(data)
-        return self._tree
+        self._parse_node(data, 0)
 
-    def _parse_node(self, data):
+    def _parse_node(self, data, node):
         num_children = data.pop(0)
         num_metadata = data.pop(0)
-        current_node = self._node
-        self._tree[current_node] = [[], []]
+        self._tree[node] = [[], []]
         # Add to children list
         for n in range(num_children):
-            self._tree[current_node][0].append(current_node + n + 1)
+            self._max_node += 1
+            self._tree[node][0].append(self._max_node)
         # Parse children nodes
-        for n in range(num_children):
-            self._node += 1
-            self._parse_node(data)
+        for child in self._tree[node][0]:
+            self._parse_node(data, child)
+        # Save metadata
         for n in range(num_metadata):
             metadata = data.pop(0)
-            self._tree[current_node][1].append(metadata)
+            self._tree[node][1].append(metadata)
 
     def sum_metadata(self):
         result = 0
@@ -29,6 +28,23 @@ class Tree:
             metadata = node_data[1]
             result += sum(metadata)
         return result
+
+    def is_leaf(self, node):
+        if node not in self._tree:
+            raise KeyError("Node not in tree.")
+        return len(self._tree[node][0]) == 0
+
+    def get_value(self, node):
+        if self.is_leaf(node):
+            # Value is the sum of its metadata
+            return sum(self._tree[node][1])
+        # Otherwise, metadata are indexes
+        value = 0
+        for index in self._tree[node][1]:
+            index -= 1
+            if 0 <= index < len(self._tree[node][0]):
+                value += self.get_value(self._tree[node][0][index])
+        return value
 
 
 def main():
@@ -39,6 +55,9 @@ def main():
     tree = Tree()
     tree.build_tree(nums)
     print(tree.sum_metadata())
+
+    # Part 2
+    print(tree.get_value(0))
 
 
 if __name__ == "__main__":
