37238de240482719644b58197c087aad3675e284
monzo94 <dmonzonis@protonmail.com>
2019-04-12 17:39:17 +0200

    Add day 14 part 2

--- day14/day14.py
+++ day14/day14.py
@@ -4,26 +4,41 @@ class Recipes:
         # Indexes currently being worked with
         self.elf1 = 0
         self.elf2 = 1
-    
+
     def advance_elves(self):
-        self.elf1 = (self.elf1 + 1 + self.recipes[self.elf1]) % len(self.recipes)
-        self.elf2 = (self.elf2 + 1 + self.recipes[self.elf2]) % len(self.recipes)
+        self.elf1 = (self.elf1 + 1 +
+                     self.recipes[self.elf1]) % len(self.recipes)
+        self.elf2 = (self.elf2 + 1 +
+                     self.recipes[self.elf2]) % len(self.recipes)
 
     def combine_step(self):
         first = self.recipes[self.elf1]
         second = self.recipes[self.elf2]
-        self.recipes += combine_recipes(first, second)
+        self.recipes += Recipes.combine_recipes(first, second)
         self.advance_elves()
 
     def get_subset(self, start, count=10):
         return self.recipes[start:start + count]
 
-    def __repr__(self):
-        return ",".join(str(x) for x in self.recipes)
+    def find_pattern(self, pattern, start=0):
+        string = ''.join(str(c) for c in self.recipes[start:])
+        return string.find(pattern)
+
+    @staticmethod
+    def combine_recipes(first, second):
+        return [int(x) for x in list(str(first + second))]
+
+    def __len__(self):
+        return len(self.recipes)
 
 
-def combine_recipes(first, second):
-    return [int(x) for x in list(str(first + second))]
+def find_recipes_before_pattern(recipes, pattern):
+    found = recipes.find_pattern(pattern)
+    while found == -1:
+        recipes.combine_step()
+        # Search from -len(pattern) - 1 in case 2 nums were added
+        found = recipes.find_pattern(pattern, -len(pattern) - 1)
+    return found + len(recipes) - len(pattern) - 1
 
 
 def main():
@@ -32,10 +47,14 @@ def main():
 
     # Part 1
     # Combine until we have enough results
-    while len(recipes.recipes) < inp + 10:
+    while len(recipes) < inp + 10:
         recipes.combine_step()
-    
-    print("".join(str(x) for x in recipes.get_subset(inp)))
+
+    print(''.join(str(c) for c in recipes.get_subset(inp)))
+
+    # Part 2
+    pattern = str(inp)
+    print(find_recipes_before_pattern(recipes, pattern))  # 172962059
 
 
 if __name__ == "__main__":
